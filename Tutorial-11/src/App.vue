9268ebb96198f78681a4b29f45425bcb73cae115
poorva-vishnoi <poorva.vishnoi@corevaluetech.com>
2026-05-20 15:06:04 +0530

    Tutorial-11

---
+++ Tutorial-11/src/App.vue
@@ -0,0 +1,48 @@
+<script>
+import BlogPost from "./components/BlogPost.vue";
+export default {
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: null,
+          author: { name: "abc" },
+          tag: [1, 2, 3],
+          marks: 98
+        },
+        {
+          id: 2,
+          title: "Blogging with Vue",
+          author: { name: "def" },
+          tag: [4, 5, 6],
+        },
+        {
+          id: 3,
+          title: "Why Vue is so fun",
+          author: { name: "ghi" },
+          tag: [7, 8, 9],
+          marks: 56
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <BlogPost
+    v-for="post in posts"
+    :id="post.id"
+    :title="post.title"
+    :isValid="true"
+    :author="post.author"
+    :tag="post.tag"
+    :marks="post.marks"
+  />
+</template>
+
+<style scoped></style>
---
+++ Tutorial-11/src/components/BlogPost.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    // Multiple possible types
+    // Required but nullable string
+    title: {
+      type: [String, null],
+      required: true,
+    },
+    // Required string
+    id: {
+      type: Number,
+      required: true,
+    },
+    // Number with a default value
+    marks:{
+      type: Number,
+      default: 100,
+    },
+    isValid: Boolean,
+    author: Object,
+    tag: Array,
+  },
+  computed: {
+    transform() {
+      return this.title.toLowerCase();
+    },
+  },
+};
+</script>
+
+<template>
+  <h4>{{ id }} {{ title }} {{ isValid }}</h4>
+  <h4>{{ author }}</h4>
+  <h4>{{ tag }}</h4>
+  <h4>{{ marks }}</h4>
+</template>
